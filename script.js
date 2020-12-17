f68cf9f9d8f95ff22d00fead2b55395e75fb65e2
srikanth <kasireddysrikanth27@gmail.com>
2020-12-17 16:28:23 +0530

    finish

---
+++ script.js
@@ -0,0 +1,24 @@
+popup = window.open(
+    "https://accounts.spotify.com/authorize?client_id=4d291bcaed1146129a83651d3a2cf801&redirect_uri=https://vibrant-wescoff-46ee41.netlify.app/&response_type=code",
+    'Login with Spotify',
+    'width=800,height=600'
+  )
+
+  window.spotifyCallback = (payload) => {
+    popup.close()
+  
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {
+        'Authorization': `Bearer ${payload}`
+      }
+    }).then(response => {
+      return response.json()
+    }).then(data => {
+      // do something with data
+    })
+  }
+  token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+
+if (token) {
+  window.opener.spotifyCallback(token)
+}
